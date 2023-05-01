2e1fd044efe4ca0b1a3fd531163a289496b69bbe
hungr <hungryfactory@icloud.com>
2023-05-01 21:00:58 +0900

    intro.refs

---
+++ src/pages/intro.refs/index.tsx
@@ -0,0 +1,23 @@
+
+export default function Refs() {
+  return (
+    <div>
+        <h1>2   Normative references</h1>
+        <p>
+            下記の文献はこの文書の内容を作成する上で一部または全てを参照した文書です。
+            時間がたった参考文献はその引用された版を採用します。更新された参考文献に関しては、最新版を採用します。
+            <ul>
+                <li>ISO/IEC 2382, Information technology</li>
+                <li>ISO 8601:2004, Data elements and interchange formats --- Information interchange --- Representation of dates and times</li>
+                <li>ISO/IEC 9899:2018, Programming languages --- C</li>
+                <li>ISO/IEC/IEEE 9945:2009, Infomation Technology --- Portable Operating System Interface (POSIX)</li>
+                <li>ISO/IEC/IEEE 9945:2009/Cor 1:2013, Information Technology --- Portable Operating System Interface (POSIX), </li>
+                <li>ISO/IEC/IEEE 60559:2020, Information technology --- Microprocessor Systems --- Floating-Point arithmetic</li>
+                <li>ISO 80000-2:2009, Quantities and units --- Part 2: Mathematical signs and symbols to be used in the natural sciences and technology</li>
+                <li>Ecma International, ECMAScript Language Specification, Standard Ecma-262, third edition, 1999.</li>
+                <li>The Unicode Consortium. The Unicode Standard. Available from: <a href="https://www.unicode.org/versions/latest">https://www.unicode.org/versions/latest</a></li>
+            </ul>
+        </p>
+    </div>
+  )
+}
