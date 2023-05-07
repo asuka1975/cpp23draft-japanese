99d7127bd2f90c0b98ba0428b53a2ea3bb897c5f
hungr <hungryfactory@icloud.com>
2023-05-07 14:19:14 +0900

    character

---
+++ src/pages/intro.defs/defns.character.container.tsx
@@ -0,0 +1,33 @@
+import Field from "@/components/Field"
+import HeadItem from "@/components/HeadItem"
+import { ReactNode } from "react";
+
+type Props = {
+    inner: boolean;
+};
+
+const content: ReactNode = <>
+    <p>{"library"} クラスまたは文字を表現するために使われる型</p>
+    <p>[Note 1: string, iostream, 正規表現クラスのテンプレートパラメータの一つとして使われます. ]</p>
+</>
+
+const DefnsCharacterContainer: React.FC<Props> = ({ inner = false }) => {
+    return (
+        <div id="defns.character container type">
+        {
+            inner ? 
+                <div>
+                    <HeadItem level={4} index="3.10" title="defns.character.container" link="/intro.defs/character container type" linkString="character container type" />
+                    {content}
+                </div> :
+                <Field>
+                    <HeadItem level={1} index="3" title="Terms and definitions" link="/" linkString="intro.defs" />
+                    <HeadItem level={4} index="3.10" title="defns.character.container" link="/intro.defs/character container type" linkString="character container type" />
+                    {content}
+                </Field>
+        }
+        </div>
+    )
+}
+
+export default DefnsCharacterContainer;
---
+++ src/pages/intro.defs/defns.character.tsx
@@ -0,0 +1,34 @@
+import Field from "@/components/Field"
+import HeadItem from "@/components/HeadItem"
+import { ReactNode } from "react";
+
+type Props = {
+    inner: boolean;
+};
+
+const content: ReactNode = <>
+    <p>{"<library>"} 連続的に扱ったときにテキストとして解釈できるオブジェクト</p>
+    <p>[Note 1: この用語は、char, char8_t, char16_t, char32_tに限らず、<a href="/strings">[strings]</a>, <a href="/location">[localization]</a>, <a href="/input.output">[input.output]</a>, or <a href="/re">[re]</a>で指定される定義を備えた型によって表現できる型すべてを意味します。]</p>
+</>
+
+
+const DefnsCharacter: React.FC<Props> = ({ inner = false }) => {
+    return (
+        <div id="defns.character">
+        {
+            inner ? 
+                <div>
+                    <HeadItem level={4} index="3.9" title="defns.character" link="/intro.defs/character" linkString="character" />
+                    {content}
+                </div> :
+                <Field>
+                    <HeadItem level={1} index="3" title="Terms and definitions" link="/" linkString="intro.defs" />
+                    <HeadItem level={4} index="3.9" title="defns.character" link="/intro.defs/character" linkString="character" />
+                    {content}
+                </Field>
+        }
+        </div>
+    )
+}
+
+export default DefnsCharacter;
