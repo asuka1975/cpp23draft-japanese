41a6719a0d4cb78f571ab0a061568f526baf573f
hungr <hungryfactory@icloud.com>
2023-05-23 23:53:14 +0900

    4.41

--- src/pages/intro.defs/defns.regex.primary.equivalence.class.tsx
+++ src/pages/intro.defs/defns.regex.primary.equivalence.class.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 const content: ReactNode = <>
-
+    <p>{"<正規表現>"} 同じ主要なソートキー、すなわち、アクセントや大文字・小文字、ロケール固有の調整ではなく文字の形のみで重み付けするものを共有する1つ以上の文字の集合</p>
 </>
 
 const DefnsRegexPrimaryEquivalenceClass: React.FC<Props> = ({ inner = false }) => {
