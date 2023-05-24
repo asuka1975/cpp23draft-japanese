0afb9fe917055986078ac793451e5f6a00636394
hungr <hungryfactory@icloud.com>
2023-05-24 14:40:33 +0900

    3.44

--- src/pages/intro.defs/defns.projection.tsx
+++ src/pages/intro.defs/defns.projection.tsx
@@ -1,3 +1,4 @@
+import CppCode from "@/components/CppCode";
 import Field from "@/components/Field"
 import HeadItem from "@/components/HeadItem"
 import { ReactNode } from "react";
@@ -7,7 +8,17 @@ type Props = {
 };
 
 const content: ReactNode = <>
-
+    <p>{"<library>"} アルゴリズムを要素の値を検査する前に適用する変換</p>
+    <p>[Example 1:</p>
+    <CppCode>
+        {`std::pair<int, std::string_view> pairs[] = {{2, "foo"}, {1, "bar"}, {0, "baz"}};
+std::ranges::sort(pairs, std::ranges::less{}, [](auto const& p) { return p.first; });`}
+    </CppCode>
+    <p>ペアの最初のメンバに基づいて昇順にソート</p>
+    <CppCode>
+        {`{{0, "baz"}, {1, "bar"}, {2, "foo"}}`}
+    </CppCode>
+    <p>]</p>
 </>
 
 const DefnsProjection: React.FC<Props> = ({ inner = false }) => {
